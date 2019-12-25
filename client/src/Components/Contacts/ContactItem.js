ca0b8c4533dbb39b374edcbf9c2974f648bc028a
Farukh1x95 <farook1x95@gmail.com>
2019-12-26 02:40:08 +0530

    Add contacts Working

--- client/src/Components/Contacts/ContactItem.js
+++ client/src/Components/Contacts/ContactItem.js
@@ -4,43 +4,62 @@ import {
   ListItemText,
   ListItem,
   Avatar,
-  Typography
+  Typography,
+  IconButton,
+  Grid
 } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: "100%",
-    maxWidth: 560,
     display: "flex",
     justifyContent: "center"
   },
   inline: {
     display: "inline-block"
+  },
+  DeleteIcon: {
+    color: "red"
+  },
+  paddinleft2: {
+    paddingLeft: theme.spacing(3)
   }
 }));
-const ContactItem = () => {
+const ContactItem = ({ SingleContact }) => {
+  const { id, name, email, phone } = SingleContact;
   const classes = useStyles();
   return (
-    <ListItem button className={classes.root}>
-      <Avatar>F</Avatar>
-      <ListItemText
-        inset
-        primary="Brunch this weekend?"
-        secondary={
-          <React.Fragment>
-            <Typography
-              component="span"
-              variant="body2"
-              className={classes.inline}
-              color="textPrimary"
-            >
-              Ali Connors
-            </Typography>
-            {" — I'll be in your neighborhood doing errands this…"}
-          </React.Fragment>
-        }
-      />
-    </ListItem>
+    <Grid>
+      <ListItem button className={classes.root}>
+        <Avatar>F</Avatar>
+        <ListItemText
+          className={classes.paddinleft2}
+          inset
+          primary={name}
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                {phone}
+              </Typography>
+              {` — ${email}`}
+            </React.Fragment>
+          }
+        />
+        <IconButton xs={4} aria-label="edit">
+          <EditIcon />
+        </IconButton>
+        <IconButton xs={4} aria-label="delete" className={classes.DeleteIcon}>
+          <DeleteIcon />
+        </IconButton>
+      </ListItem>
+    </Grid>
   );
 };
 export default ContactItem;
