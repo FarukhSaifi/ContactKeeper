eae13839a32dda99566ab29238094b56b9f421db
Farukh1x95 <farook1x95@gmail.com>
2019-12-30 00:51:55 +0530

    Add Filter Contacts

--- client/src/Components/Contacts/Contacts.js
+++ client/src/Components/Contacts/Contacts.js
@@ -6,17 +6,34 @@ import ContactItem from "./ContactItem";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Empty</h4>;
+  }
   return (
     <Fragment>
       <List component="div">
-        {console.log(contacts)}
-        {contacts.map(SingleContact => (
-          <Fragment>
-            <ContactItem key={SingleContact.id} SingleContact={SingleContact} />
-            <Divider />
-          </Fragment>
-        ))}
+        {filtered !== null
+          ? filtered.map(SingleContact => (
+              <Fragment>
+                <ContactItem
+                  key={SingleContact.id}
+                  SingleContact={SingleContact}
+                />
+                <Divider />
+              </Fragment>
+            ))
+          : contacts.map(SingleContact => (
+              <Fragment>
+                <ContactItem
+                  key={SingleContact.id}
+                  SingleContact={SingleContact}
+                />
+
+                <Divider />
+              </Fragment>
+            ))}
       </List>
     </Fragment>
   );
--- client/src/Components/Layout/Home.js
+++ client/src/Components/Layout/Home.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import Contacts from "../Contacts/Contacts";
 import ContactForm from "../Contacts/ContactForm";
+import ContactFilter from "./ContactFilter";
 
 const Home = () => {
   return (
     <div>
       <Grid container>
-        <Grid item xs={6} sm={12} md={6}>
+        <Grid item xs={12} sm={12} md={6}>
           <Contacts />
         </Grid>
-        <Grid item xs={6} sm={12} md={6}>
+        <Grid item xs={12} sm={12} md={6}>
+          <ContactFilter />
           <ContactForm />
         </Grid>
       </Grid>
